fd660c528c793288b4d50117a5bb1cf52c5873c9
Ali Ziyaan Momin <alimomin@utexas.edu>
2020-01-05 22:28:39 -0600

    added test controller

---
+++ src/main/java/com/ant/backendservices/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ant.backendservices.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Object> getTestData() {
+        String hello = "hello";
+        return new ResponseEntity<>(hello, HttpStatus.OK);
+    }
+}
